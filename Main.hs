fdd7c242dee767b79914b8fc749493762f876fac
raatmarien <raatmarien@users.noreply.github.com>
2014-12-21 14:37:59 +0100

    Fully implement timed actions
    The listen loop now continues after 100 ms even if it doesn't receive
    input, enabling the possibility of output without any activity in the
    channel.

--- Main.hs
+++ Main.hs
@@ -28,44 +28,48 @@ userMessagesFile = "userMessages.sav"
 
 main :: IO ()
 main = do
-    h <- connectTo server (PortNumber (fromIntegral port))
-    hSetBuffering h NoBuffering
-    write h (T.pack "NICK") nick
-    write h (T.pack "USER") usermsg
-    write h (T.pack "JOIN") chan
-    usrMessages <- readUserMessagesFromFile userMessagesFile
-    let emptyState = IRCState usrMessages S.empty []
-    listen h emptyState
+  h <- connectTo server (PortNumber (fromIntegral port))
+  hSetBuffering h NoBuffering
+  write h (T.pack "NICK") nick
+  write h (T.pack "USER") usermsg
+  write h (T.pack "JOIN") chan
+  usrMessages <- readUserMessagesFromFile userMessagesFile
+  let emptyState = IRCState usrMessages S.empty []
+  listen h emptyState
 
 write :: Handle -> T.Text -> T.Text -> IO ()
 write h s t = do
-    TIO.hPutStr h $ T.concat [s, T.pack " ", t, T.pack "\r\n"]
-    TIO.putStrLn $ T.concat [T.pack ">  ", s, T.pack " ", t]
+  TIO.hPutStr h $ T.concat [s, T.pack " ", t, T.pack "\r\n"]
+  TIO.putStrLn $ T.concat [T.pack ">  ", s, T.pack " ", t]
 
 listen :: Handle -> IRCState -> IO ()
 listen h st = do
-    t <- TIO.hGetLine h
-    let s = T.init t
-    nst <- eval h s st
+  inputAvailable <- hWaitForInput h 100
+  nst <- (if inputAvailable
+          then (do
+                   t <- TIO.hGetLine h
+                   let s = T.init t
+                   nst <- eval h s st
+                   TIO.putStrLn s
+                   return nst)
 
-    time <- getCurrentTime
+          else return st)
+  time <- getCurrentTime
 
-    let (IRCState usrMessages usrs timedActs) = nst
-        (IRCState oldUsrMessages _ _) = st
-        (tellActs, newUsrMessages) = tellAll usrs usrMessages
+  let (IRCState usrMessages usrs timedActs) = nst
+      (IRCState oldUsrMessages _ _) = st
+      (tellActs, newUsrMessages) = tellAll usrs usrMessages
 
-        (timedActsToRun,newTimedActs) = updateTimedActions time timedActs
-        nst' = IRCState newUsrMessages usrs newTimedActs
+      (timedActsToRun,newTimedActs) = updateTimedActions time timedActs
+      nst' = IRCState newUsrMessages usrs newTimedActs
 
-    runActs h tellActs
-    runActs h timedActsToRun
+  runActs h tellActs
+  runActs h timedActsToRun
 
-    when (newUsrMessages /= oldUsrMessages)
-      $ writeUserMessagesToFile userMessagesFile newUsrMessages
+  when (newUsrMessages /= oldUsrMessages)
+    $ writeUserMessagesToFile userMessagesFile newUsrMessages
+  listen h nst'
 
-
-    TIO.putStrLn s
-    listen h nst'
 
 eval :: Handle -> T.Text -> IRCState -> IO IRCState
 eval h s ircSt = do
