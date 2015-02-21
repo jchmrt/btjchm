4d930842ad7e9af1cc00781308c10619e09634bb
Jochem Raat <jochemraat@gmail.com>
2015-02-21 14:12:14 +0100

    Add safer saving
    Saving is now done strictly, hopefully this will ensure that we will no
    longer write to files we are already reading from.

--- Save.hs
+++ Save.hs
@@ -5,11 +5,12 @@ module Save ( save
 import Core
 import Binary
 import Data.Binary
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import Control.Applicative ((<$>))
 
 save :: FilePath -> IRCState -> IO ()
-save fp = L.writeFile fp . encode
+save fp = B.writeFile fp . L.toStrict . encode
 
 retrieve :: FilePath -> IO IRCState
 retrieve fp = decode <$> L.readFile fp
